d756b86badf908b8db89c0b6a8ec443583f85822
CryptoKarthik-369 <karthikraj.kumaresan2001@gmail.com>
2026-08-02 19:28:07 +0530

    Add files via upload

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,1 +1,25 @@
+import axios from 'axios'
 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  timeout: 15000,
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    if (error.response?.status === 401) {
+      // TODO: refresh token flow via /auth/refresh
+      localStorage.removeItem('access_token')
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
