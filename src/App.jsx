0b90ead27710d96f127bd306921714cc6ddb2f14
CryptoKarthik-369 <karthikraj.kumaresan2001@gmail.com>
2026-08-02 19:24:37 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Layout from './layouts/Layout.jsx'
+import AppRoutes from './routes/AppRoutes.jsx'
+
+export default function App() {
+  return (
+    <Layout>
+      <AppRoutes />
+    </Layout>
+  )
+}
